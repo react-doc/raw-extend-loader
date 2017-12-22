619f50fd05c109b30932894552f0eb55ee33d17b
jaywcjlove <398188662@qq.com>
2017-12-22 20:55:14 +0800

    Add file.

---
+++ README.md
@@ -0,0 +1,69 @@
+
+<div align="center">
+  <img width="200" height="200"
+    src="https://cdn3.iconfinder.com/data/icons/lexter-flat-colorfull-file-formats/56/raw-256.png">
+  <a href="https://github.com/webpack/webpack">
+    <img width="200" height="200"
+      src="https://webpack.js.org/assets/icon-square-big.svg">
+  </a>
+  <h1>Raw Extend Loader</h1>
+  <p>A loader for webpack that lets you import files as a string, [raw-loader](https://github.com/webpack-contrib/raw-loader) enhanced.</p>
+</div>
+
+<h2 align="center">Install</h2>
+
+```bash
+npm install --save-dev raw-extend-loader
+```
+
+<h2 align="center">Usage</h2>
+
+Use the loader either via your webpack config, CLI or inline.
+
+### Via webpack config (recommended)
+
+**webpack.config.js**
+```js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: [
+          {
+            loader: require.resolve('raw-extend-loader'),
+            options: {
+              dir: '/path/md',
+              filename: 'dir', // "underline | dir"
+              sep: '___',
+            },
+          },
+        ],
+      }
+    ]
+  }
+}
+```
+
+**In your application**
+```js
+import txt from './file.txt';
+```
+
+### CLI
+
+```bash
+webpack --module-bind 'txt=raw-extend-loader'
+```
+
+**In your application**
+```js
+import txt from 'file.txt';
+```
+
+### Inline
+
+**In your application**
+```js
+import txt from 'raw-extend-loader!./file.txt';
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Kenny Wang @jaywcjlove
+*/
+
+const loaderUtils = require('loader-utils');
+const FS = require('fs');
+const PATH = require('path');
+const mkdirp = require('mkdirp');
+
+module.exports = function (content) {
+  const options = loaderUtils.getOptions(this) || {}
+  const { dir, filename, sep = "dir" } = options;
+  this.cacheable && this.cacheable();
+  this.value = content;
+  const cb = this.async();
+  Promise.resolve().then(() => {
+    if (dir) {
+      let filenameString = this.resourcePath.replace(this.context + PATH.sep, '');
+      let writePath = PATH.resolve(process.cwd(), dir);
+      if (filename && filename === 'underline') {
+        const underlineFileName = this.resourcePath.replace(process.cwd() + PATH.sep, '').split(PATH.sep).join(sep || '__')
+        writePath = PATH.resolve(writePath, underlineFileName);
+      } else {
+        writePath = PATH.join(writePath, this.resourcePath.replace(process.cwd() + PATH.sep, ''));
+      }
+      mkdirp(PATH.dirname(writePath), (err) => {
+        FS.writeFile(writePath, JSON.stringify(content), (err) => {
+          if (err) {
+            this.emitError('\r\nWrite to directory failed: ' + err);
+            return cb(err);
+          }
+          cb(null, "module.exports = " + JSON.stringify(content));
+        })
+      })
+    } else {
+      cb(null, "module.exports = " + JSON.stringify(content));
+    }
+  }).catch((err) => {
+    return err ? cb(new SyntaxError(err)) : cb(err)
+  })
+}
+module.exports.seperable = true;
